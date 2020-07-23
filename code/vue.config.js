cb33b1ac90153ac3da29d129fc29d5a473350f10
Manoel Motoso <manoel.s.motoso@gmail.com>
2020-07-23 14:59:37 -0300

    deploy config

--- code/vue.config.js
+++ code/vue.config.js
@@ -20,19 +20,18 @@ module.exports = {
       awsProfile: "default",
       endpoint: "AWS",
       region: "sa-east-1",
-      bucket: "xcavemis-site",
-      // bucket: "xcave-mis-site-hml",
+      bucket: process.env.VUE_APP_S3_BUCKET,
       createBucket: true,
       staticHosting: true,
       staticIndexPage: "index.html",
       staticErrorPage: "index.html",
       assetPath: "dist",
       assetMatch: "**",
-      deployPath: "/hml",
+      deployPath: "/",
       acl: "public-read",
       pwa: false,
       pwaFiles: "Comma-separated list of files to treat as PWA files",
-      enableCloudfront: false,
+      enableCloudfront: true,
       cloudfrontId: process.env.VUE_APP_CLOUDFRONT_ID,
       cloudfrontMatchers: "/*",
       uploadConcurrency: 5,
@@ -42,6 +41,12 @@ module.exports = {
       registry: undefined,
       overrideEndpoint: false,
       pluginVersion: "4.0.0-rc3"
+    },
+    s3DeployCleanup: {
+      cleanupTag: {
+        Key: "DeployLifecycle",
+        Value: "DeleteMe"
+      }
     }
   }
 }

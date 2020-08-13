c509465c3730ffdb64d29af5d0bed77b37fef478
Caio Bassetti <caio.bassetti@vzlab.com.br>
2020-08-12 23:07:48 -0300

    Projection's content

--- code/src/data/content/5-projection.js
+++ code/src/data/content/5-projection.js
@@ -2,19 +2,19 @@ export const projection = {
     scenes: [
       {
         id: "D-501",     
-        title: "D-501",
+        title: "Tanque",
         type: 'content',
-        image: ["101/la-bele-feroniere.jpg"],
-        audio: '101/guide',
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum."
+        image: ["5-projection/D501.jpg"],
+        audio: '5-projection/A501.mp3',
+        text: "<H2><em>Manoscritto B, f. 1030</em></H2><p>Leonardo projetou uma enorme arma ofensiva: um ve&iacute;culo blindado capaz de se mover em qualquer dire&ccedil;&atilde;o e com canh&otilde;es por todos os lados. Um soldado ficava sentado na torre para dar instru&ccedil;&otilde;es.<br />Para mov&ecirc;-lo, oito homens dentro do tanque giravam as manivelas presas &agrave;s rodas de rolar que, por sua vez, estavam presas &agrave;s quatro rodas grandes. <br />No desenho de Leonardo, as rodas eram engrenadas para girar em dire&ccedil;&otilde;es opostas. Mas quem carregaria e dispararia os canh&otilde;es? O conceito de ve&iacute;culo blindado, apoiado por soldados de infantaria, foi concretizado durante a Primeira Guerra Mundial.</p>"
       },
       {
         id: "D-502",     
-        title: "D-502",
+        title: "Besta Gigante",
         type: 'content',
-        image: ["101/la-bele-feroniere.jpg"],
-        audio: '101/guide',
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sapien elit, fermentum aliquet malesuada tempus, aliquet sodales magna. Phasellus ultrices mattis bibendum."
+        image: ["5-projection/D502.jpg"],
+        audio: '',
+        text: "<h2><em>C&oacute;dice Atlanticus, f. 53 v.-ab</em></h2><p>Quanto ao tamanho dessa besta, Leonardo escreve que &ldquo;os bra&ccedil;os abertos t&ecirc;m 42 comprimentos de bra&ccedil;o&rdquo; ou aproximadamente 24 metros (as cordas esticadas) e &eacute; montado em um carrinho &ldquo;com dois bra&ccedil;os (1,1 metro) de largura e quarenta bra&ccedil;os (22,8 metros) de comprimento&rdquo;. O tamanho do arqueiro no diagrama d&aacute; uma ideia das propor&ccedil;&otilde;es. A corda do arco &eacute; puxada bem esticada por um mecanismo com parafuso, conforme mostrado no canto inferior direito do diagrama. As rodas se inclinam para proporcionar uma base mais est&aacute;vel para o disparo.</p>"
       },
       {
         id: "P-501",     
